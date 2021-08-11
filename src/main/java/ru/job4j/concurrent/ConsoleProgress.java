4233de3f5c307b3af8ebaf7e5cffbf2954100b6d
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-11 16:41:06 +0500

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        String[] process = {"\\", "|", "/"};
+        int flag = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (flag == 3) {
+                    flag = 0;
+                }
+                System.out.print("\r load: " + process[flag++]);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+    }
+}
