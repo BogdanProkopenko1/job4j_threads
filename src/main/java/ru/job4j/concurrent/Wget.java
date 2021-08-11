1f82507ae46c5aaab3a9dc5fa5a8491eaed59e25
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-11 11:52:13 +0500

    1.2. Режим ожидания. [#231217]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,7 +3,7 @@ package ru.job4j.concurrent;
 public class Wget {
 
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i < 100; i++) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException interruptedException) {
