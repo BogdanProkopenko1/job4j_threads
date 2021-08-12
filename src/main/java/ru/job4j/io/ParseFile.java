6d9468ceb75d00db6aaf8ca8534364cbeaa0ff1c
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-12 15:18:37 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final File file;
+
+    public ParseFile(String dir) {
+        file = Path.of(dir).toFile();
+    }
+
+    public String getContent(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (InputStream i = new FileInputStream(file)) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append(data);
+                }
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return output.toString();
+    }
+}
